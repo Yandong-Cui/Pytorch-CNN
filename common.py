00d596c9e6e48b45770fe6892d1f2a96ee3d9322
Yandong-Cui <13383639466@163.com>
2025-09-29 16:40:04 +0800

    Create common.py
    生成验证码数据集

---
+++ common.py
@@ -0,0 +1,16 @@
+import os
+import random
+import time
+
+captcha_array=list("0123456789abcdefghijklmnopqrstuvwxyz")
+captcha_size=4
+from captcha.image import ImageCaptcha
+if __name__ == '__main__':
+    print(captcha_array)
+    image =ImageCaptcha()
+    for i in range(20000):
+        image_val = "".join(random.sample(captcha_array, 4))
+
+        image_name = "D:\\app for myself\\pycharm\\验证码识别\Bili\\train\\{}_{}.png".format(image_val, int(time.time()))
+        print(image_name)
+        image.write(image_val,image_name)
